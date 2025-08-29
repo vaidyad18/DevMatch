bf7a3c9297b4eed3f388233317d8146f1cdbeef9
Vaidya Dandriyal <vaidyadandriyal04@gmail.com>
2025-08-29 23:04:14 +0530

    userSchema index

--- src/models/user.js
+++ src/models/user.js
@@ -79,6 +79,8 @@ const userSchema = new mongoose.Schema(
   }
 );
 
+userSchema.index({firstName:1, lastName:1});
+
 userSchema.methods.getJWT = async function () {
   const user = this;
   const token = jwt.sign({ _id: user._id }, "Vaidya@18#",{ expiresIn: '1d' });
