d85dbb8a456a996fb6ba3c1eed38d5ca54fb9d13
Vaidya Dandriyal <vaidyadandriyal04@gmail.com>
2025-09-03 17:16:19 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -5,9 +5,11 @@ const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const userRouter = require("./routes/user");
 const requestRouter = require("./routes/request");
+const cors = require("cors");
 
 const connectDB = require("./config/database");
 
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
