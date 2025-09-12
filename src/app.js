1418c95b9faeae247660af7e6632749234c4c7a1
Vaidya Dandriyal <vaidyadandriyal04@gmail.com>
2025-09-13 00:38:57 +0530

    changed base url

--- src/app.js
+++ src/app.js
@@ -20,11 +20,11 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/",authRouter);
-app.use("/api/",profileRouter);
-app.use("/api/",requestRouter);
-app.use("/api/",userRouter);
-app.use("/api/",paymentRouter);
+app.use("/",authRouter);
+app.use("/",profileRouter);
+app.use("/",requestRouter);
+app.use("/",userRouter);
+app.use("/",paymentRouter);
 
 connectDB().then(() => {
   console.log("Database connected successfully");
